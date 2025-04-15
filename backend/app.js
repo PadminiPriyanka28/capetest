65e85c99d67476999281161437601489d4783d0b
PadminiPriyanka28 <68558596+PadminiPriyanka28@users.noreply.github.com>
2025-04-15 14:26:57 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,26 @@
+// const express = require('express');
+// const app = express();
+// require('dotenv').config();
+
+// app.use(express.json());
+
+// const userRoutes = require('./routes/userRoutes');
+// app.use('/api', userRoutes);
+
+// module.exports = app;
+
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config();
 
+const app = express();
 app.use(express.json());
-
-const userRoutes = require('./routes/userRoutes');
 app.use('/api', userRoutes);
 
-module.exports = app;
+const PORT = process.env.PORT || 4000;
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = { app, server };
